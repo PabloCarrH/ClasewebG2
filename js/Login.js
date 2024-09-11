d216f0e901a184de735588cc863d68a38db79a15
juanPabloGomezb <juanp-gomezb@unilibre.edu.co>
2024-09-11 09:59:49 -0500

    Registro cliente y login

--- js/Login.js
+++ js/Login.js
@@ -7,30 +7,49 @@ document.addEventListener('DOMContentLoaded', () => {
 
     loginForm.addEventListener('submit', async (e) => {
         e.preventDefault();
+
         const email = document.querySelector("#email").value;
         const password = document.querySelector("#password").value;
-        const Users = JSON.parse(localStorage.getItem('users')) || [];
-        
-        console.log('Usuarios:', Users); 
 
-        const validUser = Users.find(user => user.email === email && user.password === password);
-        if (!validUser) {
+        // Obtener usuarios profesionales y clientes
+        const professionalUsers = JSON.parse(localStorage.getItem('users')) || [];
+        const clientUsers = JSON.parse(localStorage.getItem('clients')) || [];
+
+        // Buscar en la lista de profesionales
+        const validProfessional = professionalUsers.find(user => user.email === email && user.password === password);
+        // Buscar en la lista de clientes
+        const validClient = clientUsers.find(user => user.email === email && user.password === password);
+
+        // Si el usuario es un profesional
+        if (validProfessional) {
+            await Swal.fire({
+                icon: 'success',
+                title: `Bienvenido(a) ${validProfessional.name}`,
+                showConfirmButton: false,
+                timer: 1500
+            });
+
+            window.location.href = 'trabajador.html';
+        }
+        // Si el usuario es un cliente
+        else if (validClient) {
+            await Swal.fire({
+                icon: 'success',
+                title: `Bienvenido(a) ${validClient.name}`,
+                showConfirmButton: false,
+                timer: 1500
+            });
+
+            window.location.href = 'cliente.html';
+        }
+        // Si no se encuentra un usuario válido
+        else {
             await Swal.fire({
                 icon: 'error',
                 title: 'Error',
                 text: 'Usuario y/o contraseña incorrectos',
                 confirmButtonText: 'Aceptar'
             });
-            return;
         }
-
-        await Swal.fire({
-            icon: 'success',
-            title: `Bienvenido(a) ${validUser.name}`,
-            showConfirmButton: false,
-            timer: 1500
-        });
-
-        window.location.href = 'dashboardPro.html';
     });
 });
