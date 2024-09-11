94dcfe93a4025cd61b32d5bbfffda1f838c8937b
Lauragodoy1711 <laurav-godoyg@unilibre.edu.co>
2024-09-11 08:31:35 -0500

    Update Registro.js

--- js/Registro.js
+++ js/Registro.js
@@ -1,1 +1,22 @@
-// Este es un archivo de ejemplo
+const registerForm = document.querySelector("#professional-form")
+registerForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const name = document.querySelector("#name-professional").value
+    const email = document.querySelector("#email-professional").value
+    const password = document.querySelector("#password-professional").value
+    const date = document.querySelector("#dob-professional").value
+    const celular = document.querySelector("#phone-professional").value
+
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    const isUserRegistered = Users.find(user => user.email === email)
+    
+    if (isUserRegistered) {
+        return alert('El usuario ya está registrado')
+    }
+
+    Users.push({ name:name, email:email, password:password, date:date, celular:celular })
+    localStorage.setItem('users', JSON.stringify(Users))
+    alert('Registro Exitoso')
+    window.location.href= 'login.html'
+})
